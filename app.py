8bffdff6c813b6806048011f17968e17bcace9bf
SheikhAmmar2004 <158827574+SheikhAmmar2004@users.noreply.github.com>
2025-10-15 12:27:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -394,4 +394,5 @@ def inject_user_status():
     }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # use Render's assigned port
+    app.run(host='0.0.0.0', port=port, debug=False)
